89028a7194e45396b5b8cbf452e8a6558255ce1b
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-02-07 13:37:43 +0100

    update resume and title

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -24,6 +24,7 @@ const Home = () => {
       })
       .then((data) => {
         dispatch(getAllProjects(data));
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
